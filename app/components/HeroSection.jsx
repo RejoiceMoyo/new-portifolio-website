00c75b113b1645c12dc82bf6ee7d204d97c4cb24
RejoiceMoyo <rejoicempoyo34@gmail.com>
2024-09-17 11:44:58 +0200

    Initial Commit

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+const HeroSection = () => {
+return (
+    <section>
+<div className="grid grid-cols-1 lg:grid-cols-12">
+    <div className="col-span-7">
+
+        <h1 className="text-white mb-4 text-4xl lg:text-5xl font-extrabold">Hellooo World, I'm Rejo Moyo</h1>
+
+        <p className="text-[#adadad] text-lg lg:text-xl">Welcome to my portfolio! I'm passionate about creating user-friendly and visually appealing web applications that make an impact. Currently pursuing a Bachelor's in Computer Science, I specialize in building fully optimized, scalable apps using technologies like Next.js and Vite, ensuring fast, efficient performance.
+
+Explore my projects and journey as I work toward becoming a skilled developer. Let's build something amazing together!
+</p>
+</div>
+</div>
+    </section>
+)
+};
+
+export default HeroSection;
